722f028d1c9950ff99ed7303ab5286fcb2f51c97
LGH <3170105339@zju.edu.cn>
2021-12-07 00:31:16 +0800

    20211206 practice

--- LeetcodeHot100/238_ProductExceptSelf/Solution.java
+++ LeetcodeHot100/238_ProductExceptSelf/Solution.java
@@ -1,5 +1,25 @@
+import java.util.Arrays;
+
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        
+        int[] output = new int[nums.length];
+        output[0] = 1;
+        int forwardProduct = 1;
+        for (int i = 1; i < nums.length; i++) {
+            forwardProduct *= nums[i - 1];
+            output[i] = forwardProduct;
+        }
+        int backwardProduct = 1;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            backwardProduct *= nums[i + 1];
+            output[i] *= backwardProduct;
+        }
+        return output;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4};
+        Solution s = new Solution();
+        System.out.println(Arrays.toString(s.productExceptSelf(nums)));
     }
 }
--- PracticeNotes.md
+++ PracticeNotes.md
@@ -2,3 +2,10 @@
 
 [toc]
 
+## 20211206，除自身以外数组的乘积
+
+[leetcode链接](https://leetcode-cn.com/problems/product-of-array-except-self/)
+
+*关键词*  : 双向遍历
+
+做过的题目，从两头各遍历一次，第一次做很难做出来，做过一次后再遇到就容易想到了。
