48a342f9f47ad27580d78173154711e4a7bc31ea
LGH <3170105339@zju.edu.cn>
2021-12-10 23:48:01 +0800

    Binary Search Dynamic Programming

--- LeetcodeHot100/300_LengthOfLTS/Solution.java
+++ LeetcodeHot100/300_LengthOfLTS/Solution.java
@@ -1,23 +1,44 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int[] dp = new int[nums.length];
-        dp[0] = 1;
-        int longestLength = 1;
-        for (int i = 1; i < dp.length; i++) {
-            int longestLengthEndHere = 1;
-            for (int j = i; j >= 0; j--) {
-                if (nums[i] > nums[j]) {
-                    longestLengthEndHere = Math.max(longestLengthEndHere, dp[j] + 1);
-                }
+        int[] tails = new int[nums.length];
+        int tailsLength = 0;
+        for (int num : nums) {
+            if (tailsLength == 0 || num > tails[tailsLength - 1]) {
+                tails[tailsLength] = num;
+                tailsLength += 1;
+            } else {
+                binarySearchAndReplace(tails, tailsLength, num);
             }
-            dp[i] = longestLengthEndHere;
-            longestLength = Math.max(longestLength, dp[i]);
         }
-        return longestLength;
+        return tailsLength;
+    }
+
+    private void binarySearchAndReplace(int[] tails, int tailsLength, int target) {
+        int l = 0, r = tailsLength - 1;
+        while (true) {
+            if (r - l <= 1) {
+                if (tails[l] >= target) {
+                    tails[l] = target;
+                } else if (tails[r] >= target) {
+                    tails[r] = target;
+                } else {
+                    tails[r + 1] = target;
+                }
+                break;
+            }
+            int middle = (l + r) / 2;
+            if (tails[middle] > target) {
+                r = middle - 1;
+            } else if (tails[middle] < target) {
+                l = middle + 1;
+            } else {
+                break;
+            }
+        }
     }
 
     public static void main(String[] args) {
-        int[] nums = {3, 3, 3, 3, 3, 3};
+        int[] nums = {0,1,0,3,2,3};
         Solution s = new Solution();
         System.out.println(s.lengthOfLIS(nums));
     }
