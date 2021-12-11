12471c8f62717529c784c93db1faec3849ca01cf
LGH <3170105339@zju.edu.cn>
2021-12-11 22:16:55 +0800

    Regular Dynamic Programming

---
+++ LeetcodeHot100/309_MaxProfit/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int[][] dp = new int[prices.length][3];
+        dp[0][0] = -1 * prices[0];
+        dp[0][1] = 0;
+        dp[0][2] = 0;
+        for (int i = 1; i < prices.length; i++) {
+            
+        }
+    }
+}
