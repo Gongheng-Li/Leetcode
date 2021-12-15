93bcaf7f752c28476419f485a9dae220ae324831
LGH <3170105339@zju.edu.cn>
2021-12-15 17:01:30 +0800

    QuickSort Solution

--- LeetcodeHot100/347_TopKFrequent/Solution.java
+++ LeetcodeHot100/347_TopKFrequent/Solution.java
@@ -1,7 +1,4 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.PriorityQueue;
+import java.util.*;
 
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
@@ -12,21 +9,61 @@ class Solution {
         for (int num : nums) {
             frequency.put(num, frequency.getOrDefault(num, 0) + 1);
         }
+        int[] uniqueNums = new int[frequency.size()];
+        int i = 0;
         for (int key : frequency.keySet()) {
-            if (heap.size() < k) {
-                heap.add(key);
+            uniqueNums[i] = key;
+            i += 1;
+        }
+//        int[] uniqueNums = {5, 3, 4, 8, 1, 6, 9, 2, 7};
+        int left = 0, right = uniqueNums.length - 1;
+        while (left <= right) {
+            int settledPosition = partition(uniqueNums, left, right, frequency);
+            if (settledPosition > k) {
+                right = settledPosition - 1;
+            } else if (settledPosition < k) {
+                left = settledPosition + 1;
             } else {
-                if (frequency.get(key) > frequency.get(heap.peek())) {
-                    heap.poll();
-                    heap.add(key);
-                }
+                break;
             }
         }
         int[] result = new int[k];
-        for (int i = 0; i < k; i++) {
-            result[i] = heap.poll();
-        }
+        System.arraycopy(uniqueNums, 0, result, 0, k);
         return result;
+    }
+
+    private int partition(int[] nums, int left, int right, Map<Integer, Integer> frequency) {
+        Random random = new Random();
+        swap(nums, left, left + random.nextInt(right - left + 1));
+        int l = left + 1, r = right;
+        while (l <= r) {
+            while (l <= right) {
+                if (frequency.get(nums[l]) <= frequency.get(nums[left])) {
+                    break;
+                }
+                l += 1;
+            }
+            while (r >= left) {
+                if (frequency.get(nums[r]) >= frequency.get(nums[left])) {
+                    break;
+                }
+                r -= 1;
+            }
+            if (l > r) {
+                break;
+            }
+            swap(nums, l, r);
+            l += 1;
+            r -= 1;
+        }
+        swap(nums, left, r);
+        return r;
+    }
+
+    private void swap(int[] nums, int x, int y) {
+        int temp = nums[x];
+        nums[x] = nums[y];
+        nums[y] = temp;
     }
 
     public static void main(String[] args) {
