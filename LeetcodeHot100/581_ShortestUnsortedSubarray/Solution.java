d02dcf7063d334bba30e254ba3ce31c3303a1d86
LGH <3170105339@zju.edu.cn>
2021-12-25 22:16:37 +0800

    Space Effective Solution

--- LeetcodeHot100/581_ShortestUnsortedSubarray/Solution.java
+++ LeetcodeHot100/581_ShortestUnsortedSubarray/Solution.java
@@ -1,36 +1,29 @@
 class Solution {
     public int findUnsortedSubarray(int[] nums) {
-        int leftEnd = 0, rightEnd = 0;
-        int[] extreme = new int[nums.length];
-        extreme[0] = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            extreme[i] = Math.max(nums[i], extreme[i - 1]);
-        }
-        for (int i = nums.length - 1; i >= 0; i--) {
-            if (extreme[i] != nums[i]) {
-                rightEnd = i;
-                break;
-            }
-        }
-        extreme[nums.length - 1] = nums[nums.length - 1];
-        for (int i = nums.length - 2; i >= 0; i--) {
-            extreme[i] = Math.min(nums[i], extreme[i + 1]);
-        }
+        int maxByThis = Integer.MIN_VALUE;
+        int minByThis = Integer.MAX_VALUE;
+        int leftEnd = -1, rightEnd = -1;
         for (int i = 0; i < nums.length; i++) {
-            if (extreme[i] != nums[i]) {
-                leftEnd = i;
-                break;
+            if (maxByThis > nums[i]) {
+                rightEnd = i;
+            } else {
+                maxByThis = nums[i];
+            }
+            if (minByThis < nums[nums.length - 1 - i]) {
+                leftEnd = nums.length - 1 - i;
+            } else {
+                minByThis = nums[nums.length - 1 - i];
             }
         }
-        if (leftEnd < rightEnd) {
-            return rightEnd - leftEnd + 1;
-        } else {
+        if (leftEnd == -1) {
             return 0;
+        } else {
+            return rightEnd - leftEnd + 1;
         }
     }
 
     public static void main(String[] args) {
-        int[] nums = {1};
+        int[] nums = {2, 6, 4, 8, 10, 9, 15};
         Solution s = new Solution();
         System.out.println(s.findUnsortedSubarray(nums));
     }
