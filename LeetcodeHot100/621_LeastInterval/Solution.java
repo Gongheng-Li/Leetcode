a7919635ae4b6a7c7f72bb163cd4240762f2bd2b
LGH <3170105339@zju.edu.cn>
2021-12-27 22:53:25 +0800

    I-Don't-Know-How-To-Describe Solution

--- LeetcodeHot100/621_LeastInterval/Solution.java
+++ LeetcodeHot100/621_LeastInterval/Solution.java
@@ -1,5 +1,26 @@
+import java.util.*;
+
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-        
+        Map<Character, Integer> charCount = new HashMap<>();
+        int maxExecuteTimes = 0;
+        int maxCount = 0;
+        for (char ch : tasks) {
+            int executeTimes = charCount.getOrDefault(ch, 0) + 1;
+            charCount.put(ch, executeTimes);
+            maxExecuteTimes = Math.max(maxExecuteTimes, executeTimes);
+        }
+        for (char ch : charCount.keySet()) {
+            if (charCount.get(ch) == maxExecuteTimes) {
+                maxCount += 1;
+            }
+        }
+        return Math.max((maxExecuteTimes - 1) * (n + 1) + maxCount, tasks.length);
+    }
+
+    public static void main(String[] args) {
+        char[] tasks = {'A', 'A', 'A', 'A', 'A', 'A', 'B', 'C', 'D', 'E', 'F', 'G'};
+        Solution s = new Solution();
+        System.out.println(s.leastInterval(tasks, 2));
     }
 }
