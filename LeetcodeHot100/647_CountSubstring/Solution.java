22e27b55f05ee82c26e6ad902c8c81079bc07c80
LGH <3170105339@zju.edu.cn>
2021-12-29 21:14:33 +0800

    Manacher Algorithm

--- LeetcodeHot100/647_CountSubstring/Solution.java
+++ LeetcodeHot100/647_CountSubstring/Solution.java
@@ -1,16 +1,28 @@
 class Solution {
     public int countSubstrings(String s) {
+        StringBuilder manacherString = new StringBuilder("$#");
+        for (int i = 0; i < s.length(); i++) {
+            manacherString.append(s.charAt(i));
+            manacherString.append('#');
+        }
+        manacherString.append('!');
+        int rightMost = 0;
+        int centerOfRightMost = 0;
         int sum = 0;
-        for (int i = 0; i < 2 * s.length() - 1; i++) {
-            int left = i / 2, right = i / 2 + i % 2;
-            while (left >= 0 && right < s.length()) {
-                if (s.charAt(left) == s.charAt(right)) {
-                    sum += 1;
-                } else {
-                    break;
-                }
-                left -= 1;
-                right += 1;
+        int[] radius = new int[2 * s.length() + 1];
+        for (int i = 0; i < 2 * s.length() + 1; i++) {
+            radius[i] = 1;
+            if (i < rightMost) {
+                radius[i] = Math.min(rightMost - i + 1, radius[centerOfRightMost * 2 - i]);
+            }
+            while (manacherString.charAt(i - radius[i] + 1 + 1) == manacherString.charAt(i + radius[i] - 1 + 1)) {
+                radius[i] += 1;
+            }
+            radius[i] -= 1;
+            sum += radius[i] / 2;
+            if (i + radius[i] - 1 > rightMost) {
+                rightMost = i + radius[i] - 1;
+                centerOfRightMost = i;
             }
         }
         return sum;
@@ -18,6 +30,6 @@ class Solution {
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.countSubstrings("aaa"));
+        System.out.println(s.countSubstrings("aaaa"));
     }
 }
