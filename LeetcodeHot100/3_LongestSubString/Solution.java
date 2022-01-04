4dc7bc3d5cd99db2cc9f11e41f9b89dc55bc6348
LGH <3170105339@zju.edu.cn>
2022-01-04 20:13:00 +0800

    Queue and HashSet

--- LeetcodeHot100/3_LongestSubString/Solution.java
+++ LeetcodeHot100/3_LongestSubString/Solution.java
@@ -1,5 +1,34 @@
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
 class Solution {
     public int lengthOfLongestSubstring(String s) {
+        Queue<Character> substring = new LinkedList<>();
+        Set<Character> characters = new HashSet<>();
+        int longest = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (characters.contains(ch)) {
+                while (!substring.isEmpty() && substring.peek() != ch) {
+                    char chInsideDuplicate = substring.poll();
+                    characters.remove(chInsideDuplicate);
+                }
+                substring.poll();
+            } else {
+                characters.add(ch);
+            }
+            substring.add(ch);
+            if (substring.size() > longest) {
+                longest = substring.size();
+            }
+        }
+        return longest;
+    }
 
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.lengthOfLongestSubstring(""));
     }
 }
