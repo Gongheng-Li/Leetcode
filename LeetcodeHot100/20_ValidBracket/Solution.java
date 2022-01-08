429d3f9d5f9217da7946326b6f3dd4981942a663
LGH <3170105339@zju.edu.cn>
2022-01-08 19:13:14 +0800

    Auxiliary Stack

--- LeetcodeHot100/20_ValidBracket/Solution.java
+++ LeetcodeHot100/20_ValidBracket/Solution.java
@@ -1,5 +1,31 @@
+import java.util.Stack;
+
 class Solution {
     public boolean isValid(String s) {
+        Stack<Character> auxiliaryStack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == '(' || ch == '[' || ch == '{') {
+                auxiliaryStack.push(ch);
+            } else if (ch == ')') {
+                if (auxiliaryStack.empty() || !auxiliaryStack.pop().equals('(')) {
+                    return false;
+                }
+            } else if (ch == ']') {
+                if (auxiliaryStack.empty() || !auxiliaryStack.pop().equals('[')) {
+                    return false;
+                }
+            } else if (ch == '}') {
+                if (auxiliaryStack.empty() || !auxiliaryStack.pop().equals('{')) {
+                    return false;
+                }
+            }
+        }
+        return auxiliaryStack.empty();
+    }
 
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.isValid("()[]{}]]"));
     }
 }
