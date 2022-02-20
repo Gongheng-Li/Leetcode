0807f61e40a983ded5c65b33d77cb5a7832a7627
LGH <3170105339@zju.edu.cn>
2022-02-20 14:15:12 +0800

    Solution With Stack

--- LeetcodeHot100/32_LongestValidParentheses/Solution.java
+++ LeetcodeHot100/32_LongestValidParentheses/Solution.java
@@ -1,23 +1,27 @@
+import java.util.Stack;
+
 class Solution {
     public int longestValidParentheses(String s) {
-        int[] dp = new int[s.length()];
         int longestLength = 0;
-        for (int i = 1; i < s.length(); i++) {
-            if (i - dp[i - 1] - 1 < 0 || s.charAt(i) == '(' || s.charAt(i - dp[i - 1] - 1) == ')') {
-                dp[i] = 0;
-                continue;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                if (stack.empty()) {
+                    stack.push(i);
+                } else {
+                    longestLength = Math.max(longestLength, i - stack.peek());
+                }
             }
-            dp[i] = dp[i - 1] + 2;
-            if (i - dp[i - 1] - 2 > 0) {
-                dp[i] += dp[i - dp[i - 1] - 2];
-            }
-            longestLength = Math.max(longestLength, dp[i]);
         }
         return longestLength;
     }
 
     public static void main(String[] args) {
-        String parentheses = "()(()";
+        String parentheses = "(()(()())";
         Solution s = new Solution();
         System.out.println(s.longestValidParentheses(parentheses));
     }
