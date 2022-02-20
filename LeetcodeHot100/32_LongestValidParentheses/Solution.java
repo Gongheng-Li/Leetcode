9a418c05293eaa7d9854b4f548a932f116e5a7c2
LGH <3170105339@zju.edu.cn>
2022-02-20 18:35:38 +0800

    Solution With The Least Space

--- LeetcodeHot100/32_LongestValidParentheses/Solution.java
+++ LeetcodeHot100/32_LongestValidParentheses/Solution.java
@@ -3,25 +3,40 @@ import java.util.Stack;
 class Solution {
     public int longestValidParentheses(String s) {
         int longestLength = 0;
-        Stack<Integer> stack = new Stack<>();
-        stack.push(-1);
+        int leftCount = 0, rightCount = 0;
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '(') {
-                stack.push(i);
+                leftCount += 1;
             } else {
-                stack.pop();
-                if (stack.empty()) {
-                    stack.push(i);
-                } else {
-                    longestLength = Math.max(longestLength, i - stack.peek());
-                }
+                rightCount += 1;
+            }
+            if (leftCount == rightCount) {
+                longestLength = Math.max(longestLength, 2 * leftCount);
+            } else if (leftCount < rightCount) {
+                leftCount = 0;
+                rightCount = 0;
+            }
+        }
+        leftCount = 0;
+        rightCount = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == '(') {
+                leftCount += 1;
+            } else {
+                rightCount += 1;
+            }
+            if (leftCount == rightCount) {
+                longestLength = Math.max(longestLength, 2 * leftCount);
+            } else if (leftCount > rightCount) {
+                leftCount = 0;
+                rightCount = 0;
             }
         }
         return longestLength;
     }
 
     public static void main(String[] args) {
-        String parentheses = "(()(()())";
+        String parentheses = "(()";
         Solution s = new Solution();
         System.out.println(s.longestValidParentheses(parentheses));
     }
