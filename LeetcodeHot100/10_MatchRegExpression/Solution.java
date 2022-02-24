9dd2f7d18382ccb3130e4d1df82a0d4f9b9e5e26
LGH <3170105339@zju.edu.cn>
2022-02-24 23:43:46 +0800

    Dynamic Programming

--- LeetcodeHot100/10_MatchRegExpression/Solution.java
+++ LeetcodeHot100/10_MatchRegExpression/Solution.java
@@ -1,69 +1,28 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-        return isMatch(s, p, 0, 0);
-    }
-
-    private boolean isMatch(String s, String p, int sStart, int pStart) {
-        int sPtr = sStart, pPtr = pStart;
-        char former = ' ';
-        while (sPtr < s.length() && pPtr < p.length()) {
-            if (s.charAt(sPtr) == p.charAt(pPtr) || p.charAt(pPtr) == '.') {
-                former = p.charAt(pPtr);
-                sPtr += 1;
-                pPtr += 1;
-            } else if (p.charAt(pPtr) == '*') {
-                if (former == '.') {
-                    while (p.charAt(pPtr) == '*' || p.charAt(pPtr) == '.') {
-                        pPtr += 1;
-                        if (pPtr >= p.length()) {
-                            return true;
-                        }
+        boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
+        dp[0][0] = true;
+        for (int i = 0; i <= s.length(); i++) {
+            for (int j = 1; j <= p.length(); j++) {
+                if (p.charAt(j - 1) == '*') {
+                    dp[i][j] = dp[i][j - 2];
+                    if (i > 0 && (s.charAt(i - 1) == p.charAt(j - 2) || p.charAt(j - 2) == '.')) {
+                        dp[i][j] |= dp[i - 1][j];
                     }
-                    char ch = p.charAt(pPtr);
-                    while (sPtr < s.length()) {
-                        if (s.charAt(sPtr) == ch && isMatch(s, p, sPtr, pPtr)) {
-                            return true;
-                        }
-                        sPtr += 1;
-                    }
-                    return false;
-                } else {
-                    pPtr += 1;
-                    int count = 0;
-                    while (sPtr < s.length() && s.charAt(sPtr) == former) {
-                        count += 1;
-                        sPtr += 1;
-                    }
-                    while (pPtr < p.length()) {
-                        if (p.charAt(pPtr) == former) {
-                            count -= 1;
-                            pPtr += 1;
-                            if (pPtr < p.length() && p.charAt(pPtr) == '*') {
-                                count += 1;
-                                pPtr += 1;
-                            }
-                            if (count < 0) {
-                                return false;
-                            }
-                        }
+                } else if (i > 0) {
+                    if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '.') {
+                        dp[i][j] = dp[i - 1][j - 1];
+                    } else {
+                        dp[i][j] = false;
                     }
                 }
-//                pPtr += 1;
-            } else if (pPtr + 1 < p.length() && p.charAt(pPtr + 1) == '*') {
-                former = p.charAt(pPtr);
-                pPtr += 2;
-            } else {
-                return false;
             }
         }
-        while (pPtr < p.length() && p.charAt(pPtr) == '*') {
-            pPtr += 1;
-        }
-        return sPtr >= s.length() && pPtr >= p.length();
+        return dp[s.length()][p.length()];
     }
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.isMatch("aaa", "ab*a*c*a"));
+        System.out.println(s.isMatch("aa", "a*"));
     }
 }
