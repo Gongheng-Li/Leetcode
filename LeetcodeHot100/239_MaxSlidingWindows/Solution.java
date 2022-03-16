2491aa7bb463ee37f88129050e509aeb6678ab21
LGH <3170105339@zju.edu.cn>
2022-03-16 20:41:38 +0800

    Partition Preprocess

--- LeetcodeHot100/239_MaxSlidingWindows/Solution.java
+++ LeetcodeHot100/239_MaxSlidingWindows/Solution.java
@@ -4,23 +4,26 @@ import java.util.Deque;
 
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        int[] max = new int[nums.length - k + 1];
-        Deque<Integer> deque = new ArrayDeque<>();
-        for (int i = 0; i < k - 1; i++) {
-            while (!deque.isEmpty() && nums[deque.peekLast()] < nums[i]) {
-                deque.removeLast();
+        int[] prefix = new int[nums.length];
+        int[] suffix = new int[nums.length];
+        suffix[nums.length - 1] = nums[nums.length - 1];
+        for (int i = 0; i < nums.length; i++) {
+            if (i % k == 0) {
+                prefix[i] = nums[i];
+            } else {
+                prefix[i] = Math.max(prefix[i - 1], nums[i]);
             }
-            deque.addLast(i);
         }
-        for (int i = k - 1; i < nums.length; i++) {
-            while (!deque.isEmpty() && nums[deque.peekLast()] < nums[i]) {
-                deque.removeLast();
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if ((i + 1) % k == 0) {
+                suffix[i] = nums[i];
+            } else {
+                suffix[i] = Math.max(suffix[i + 1], nums[i]);
             }
-            deque.addLast(i);
-            while (!deque.isEmpty() && deque.peekFirst() < i - k + 1) {
-                deque.removeFirst();
-            }
-            max[i - k + 1] = nums[deque.peekFirst()];
+        }
+        int[] max = new int[nums.length - k + 1];
+        for (int i = 0; i < max.length; i++) {
+            max[i] = Math.max(prefix[i + k - 1], suffix[i]);
         }
         return max;
     }
