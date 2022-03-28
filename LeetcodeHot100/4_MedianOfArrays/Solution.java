8206df72ad678cc2885416fd8522b80dc869115e
LGH <3170105339@zju.edu.cn>
2022-03-28 20:37:24 +0800

    Binary Search

--- LeetcodeHot100/4_MedianOfArrays/Solution.java
+++ LeetcodeHot100/4_MedianOfArrays/Solution.java
@@ -1,40 +1,44 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int[] longerArray, shorterArray;
-        if (nums1.length < nums2.length) {
-            longerArray = nums2;
-            shorterArray = nums1;
+        int totalLength = nums1.length + nums2.length;
+        if (totalLength % 2 == 0) {
+            return (findKthMin(nums1, nums2, totalLength/2) + findKthMin(nums1, nums2, totalLength/2 + 1)) / 2.0;
         } else {
-            longerArray = nums1;
-            shorterArray = nums2;
+            return findKthMin(nums1, nums2, totalLength/2 + 1);
         }
-        int longerMiddle = longerArray.length / 2, shorterMiddle = shorterArray.length / 2;
-        int leftBound = 0, rightBound = shorterArray.length - 1;
-        while (leftBound <= rightBound) {
-            if (shorterArray[shorterMiddle] > longerArray[longerMiddle + 1]) {
-                rightBound = shorterMiddle - 1;
-                longerMiddle += shorterMiddle - (leftBound + rightBound) / 2;
-                shorterMiddle = (leftBound + rightBound) / 2;
-            } else if (shorterArray[shorterMiddle] < longerArray[longerMiddle - 1]) {
-                leftBound = shorterMiddle + 1;
-                longerMiddle -= (leftBound + rightBound) / 2 - shorterMiddle;
-                shorterMiddle = (leftBound + rightBound) / 2;
-            } else {
-                break;
+    }
+
+    private int findKthMin(int[] nums1, int[] nums2, int k) {
+        int start1 = 0, start2 = 0;
+        while (true) {
+            if (start1 == nums1.length) {
+                return nums2[start2 + k - 1];
+            } else if (start2 == nums2.length) {
+                return nums1[start1 + k - 1];
             }
-        }
-        if (longerArray.length % 2 == 0 && shorterArray.length % 2 == 0) {
-            return (Math.min(longerArray[longerMiddle], shorterArray[shorterMiddle]) +
-                    Math.max(longerArray[longerMiddle - 1], shorterArray[shorterMiddle - 1])) / 2.0;
-        } else if (longerArray.length % 2 == 1 && shorterArray.length % 2 == 1) {
-            return (longerArray[longerMiddle] + shorterArray[shorterMiddle]) / 2.0;
-        } else {
-            return Math.min(longerArray[longerMiddle], shorterArray[shorterMiddle]);
+            if (k == 1) {
+                return Math.min(nums1[start1], nums2[start2]);
+            }
+//            int middle = k/2 - 1;
+            int middle1 = start1 + k/2 - 1;
+            int middle2 = start2 + k/2 - 1;
+            if (middle1 >= nums1.length) {
+                middle1 = nums1.length - 1;
+            } else if (middle2 >= nums2.length) {
+                middle2 = nums2.length - 1;
+            }
+            if (nums1[middle1] <= nums2[middle2]) {
+                k -= middle1 - start1 + 1;
+                start1 = middle1 + 1;
+            } else {
+                k -= middle2 - start2 + 1;
+                start2 = middle2 + 1;
+            }
         }
     }
 
     public static void main(String[] args) {
-        int[] nums1 = {1, 3}, nums2 = {2};
+        int[] nums1 = {2}, nums2 = {1, 3};
         Solution s = new Solution();
         System.out.println(s.findMedianSortedArrays(nums1, nums2));
     }
