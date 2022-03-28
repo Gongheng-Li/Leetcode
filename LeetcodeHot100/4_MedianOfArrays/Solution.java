1155fbce3c865cf50d60a466a9d4cc98fdf8fd85
LGH <3170105339@zju.edu.cn>
2022-03-28 20:38:08 +0800

    Dividing Arrays

--- LeetcodeHot100/4_MedianOfArrays/Solution.java
+++ LeetcodeHot100/4_MedianOfArrays/Solution.java
@@ -1,39 +1,41 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int totalLength = nums1.length + nums2.length;
-        if (totalLength % 2 == 0) {
-            return (findKthMin(nums1, nums2, totalLength/2) + findKthMin(nums1, nums2, totalLength/2 + 1)) / 2.0;
-        } else {
-            return findKthMin(nums1, nums2, totalLength/2 + 1);
+        if (nums1.length > nums2.length) {
+            return findMedianSortedArrays(nums2, nums1);
         }
-    }
-
-    private int findKthMin(int[] nums1, int[] nums2, int k) {
-        int start1 = 0, start2 = 0;
-        while (true) {
-            if (start1 == nums1.length) {
-                return nums2[start2 + k - 1];
-            } else if (start2 == nums2.length) {
-                return nums1[start1 + k - 1];
+        int left = 0, right = nums1.length;
+        int leftMax = 0, rightMin = 0;
+        int totalLength = nums1.length + nums2.length;
+        int sum = (totalLength + 1) / 2;
+        while (left <= right) {
+            int m = (left + right) / 2;
+            int n = sum - m;
+            int num1left = Integer.MIN_VALUE, num1right = Integer.MAX_VALUE;
+            int num2left = Integer.MIN_VALUE, num2right = Integer.MAX_VALUE;
+            if (m > 0) {
+                num1left = nums1[m - 1];
             }
-            if (k == 1) {
-                return Math.min(nums1[start1], nums2[start2]);
+            if (m < nums1.length) {
+                num1right = nums1[m];
             }
-//            int middle = k/2 - 1;
-            int middle1 = start1 + k/2 - 1;
-            int middle2 = start2 + k/2 - 1;
-            if (middle1 >= nums1.length) {
-                middle1 = nums1.length - 1;
-            } else if (middle2 >= nums2.length) {
-                middle2 = nums2.length - 1;
+            if (n > 0) {
+                num2left = nums2[n - 1];
             }
-            if (nums1[middle1] <= nums2[middle2]) {
-                k -= middle1 - start1 + 1;
-                start1 = middle1 + 1;
+            if (n < nums2.length) {
+                num2right = nums2[n];
+            }
+            if (num1left <= num2right) {
+                leftMax = Math.max(num1left, num2left);
+                rightMin = Math.min(num1right, num2right);
+                left = m + 1;
             } else {
-                k -= middle2 - start2 + 1;
-                start2 = middle2 + 1;
+                right = m - 1;
             }
+        }
+        if (totalLength % 2 == 0) {
+            return (leftMax + rightMin) / 2.0;
+        } else {
+            return leftMax;
         }
     }
 
