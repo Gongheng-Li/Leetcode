dab36e362f59a80ed799f8c8cfc901bad9d56dc0
LGH <3170105339@zju.edu.cn>
2022-03-31 19:38:32 +0800

    Binary Search

---
+++ PracticeByCategories/Array/704_BinarySearch/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length;
+        while (left < right) {
+            int middle = left + (right - left) / 2;
+            if (nums[middle] == target) {
+                return middle;
+            } else if (nums[middle] > target) {
+                right = middle;
+            } else {
+                left = middle + 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 3, 5, 9, 12};
+        Solution s = new Solution();
+        System.out.println(s.search(nums, 9));
+    }
+}
