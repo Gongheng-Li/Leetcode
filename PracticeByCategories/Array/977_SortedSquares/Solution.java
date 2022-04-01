6bd8b37955b7191b360478bbaffdad2974ec201a
LGH <3170105339@zju.edu.cn>
2022-04-01 20:25:00 +0800

    Double Pointers From Sides

--- PracticeByCategories/Array/977_SortedSquares/Solution.java
+++ PracticeByCategories/Array/977_SortedSquares/Solution.java
@@ -4,47 +4,22 @@ class Solution {
     public int[] sortedSquares(int[] nums) {
         int[] square = new int[nums.length];
         int left = 0, right = nums.length - 1;
-        int zero = 0;
+        int index = nums.length - 1;
         while (left <= right) {
-            zero = (left + right) / 2;
-            if (nums[zero] < 0) {
-                left = zero + 1;
+            if (Math.abs(nums[left]) < Math.abs(nums[right])) {
+                square[index] = nums[right] * nums[right];
+                right -= 1;
             } else {
-                right = zero - 1;
+                square[index] = nums[left] * nums[left];
+                left += 1;
             }
-        }
-        int ptr1 = 0, ptr2 = 0;
-        if (nums[zero] >= 0) {
-            ptr1 = zero;
-            ptr2 = zero - 1;
-        } else {
-            ptr1 = zero + 1;
-            ptr2 = zero;
-        }
-        int i = 0;
-        while (ptr1 < nums.length && ptr2 >= 0) {
-            if (Math.abs(nums[ptr1]) < Math.abs(nums[ptr2])) {
-                square[i] = nums[ptr1] * nums[ptr1];
-                ptr1 += 1;
-            } else {
-                square[i] = nums[ptr2] * nums[ptr2];
-                ptr2 -= 1;
-            }
-            i += 1;
-        }
-        for (int j = ptr2; j >= 0; j--) {
-            square[i] = nums[j] * nums[j];
-            i += 1;
-        }
-        for (int j = ptr1; j < nums.length; j++) {
-            square[i] = nums[j] * nums[j];
-            i += 1;
+            index -= 1;
         }
         return square;
     }
 
     public static void main(String[] args) {
-        int[] nums = {-1};
+        int[] nums = {-4, -1, 0, 3, 10};
         Solution s = new Solution();
         System.out.println(Arrays.toString(s.sortedSquares(nums)));
     }
