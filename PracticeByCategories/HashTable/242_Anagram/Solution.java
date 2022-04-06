9a25fa74fae1769e9e320d51bf4e9b9b35d3fa6f
LGH <3170105339@zju.edu.cn>
2022-04-06 23:23:46 +0800

    Array

---
+++ PracticeByCategories/HashTable/242_Anagram/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] count = new int[26];
+        int length = s.length();
+        for (int i = 0; i < length; i++) {
+            count[s.charAt(i) - 'a'] += 1;
+            count[t.charAt(i) - 'a'] -= 1;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
