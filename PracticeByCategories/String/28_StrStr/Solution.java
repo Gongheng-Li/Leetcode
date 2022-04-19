b6129f6f2d8cd519d6f5dcfe9b6dfeaba4994cd6
LGH <3170105339@zju.edu.cn>
2022-04-19 23:39:10 +0800

    KMP Algorithm

--- PracticeByCategories/String/28_StrStr/Solution.java
+++ PracticeByCategories/String/28_StrStr/Solution.java
@@ -34,6 +34,6 @@ class Solution {
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.strStr("hello", "ll"));
+        System.out.println(s.strStr("abcdefg", "cdfcdfcdf"));
     }
 }
