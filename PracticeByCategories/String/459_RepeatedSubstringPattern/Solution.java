1472a5fece043c434a570f9f6881a4926ff00274
LGH <3170105339@zju.edu.cn>
2022-04-24 01:00:14 +0800

    KMP Algorithm

--- PracticeByCategories/String/459_RepeatedSubstringPattern/Solution.java
+++ PracticeByCategories/String/459_RepeatedSubstringPattern/Solution.java
@@ -1,25 +1,24 @@
 class Solution {
     public boolean repeatedSubstringPattern(String s) {
-        s = " " + s;
-        char[] sArray = s.toCharArray();
-        int[] next = new int[sArray.length];
-        for (int j = 0, i = 2; i < sArray.length; i++){
-            while (j != 0 && sArray[i] != sArray[j + 1]) {
-                j = next[j];
+        int sLength = s.length();
+        int[] next = new int[sLength];
+        for (int i = 1, j = 0; i < sLength; i++) {
+            while (j > 0 && s.charAt(i) != s.charAt(j)) {
+                j = next[j - 1];
             }
-            if (sArray[i] == sArray[j + 1]) {
+            if (s.charAt(i) == s.charAt(j)) {
                 j += 1;
             }
             next[i] = j;
-            if (next[i] < next[i - 1] || next[i] == next[i - 1] && next[i] != 0) {
-                return false;
-            }
+//            if (next[i] < next[i - 1] || next[i] != 0 && next[i] == next[i - 1]) {
+//                return false;
+//            }
         }
-        return next[next.length - 1] % (next.length - next[next.length - 1] - 1) == 0;
+        return next[sLength - 1] != 0 && next[sLength - 1] % (sLength - next[sLength - 1]) == 0;
     }
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.repeatedSubstringPattern("a"));
+        System.out.println(s.repeatedSubstringPattern("abcabdasd"));
     }
 }
