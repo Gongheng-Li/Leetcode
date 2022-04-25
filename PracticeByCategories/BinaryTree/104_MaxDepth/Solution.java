ed63adf59c2a97b429ae95abb6b87dca8aaf4b28
LGH <3170105339@zju.edu.cn>
2022-04-25 20:19:51 +0800

    BFS

---
+++ PracticeByCategories/BinaryTree/104_MaxDepth/Solution.java
@@ -0,0 +1,45 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Solution {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public int maxDepth(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        int depth = 0;
+        if (root != null) {
+            queue.add(root);
+        }
+        while (!queue.isEmpty()) {
+            depth += 1;
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+        }
+        return depth;
+    }
+}
