e4f1172e6445406c7c537daf0f60a59e64784fd7
LGH <3170105339@zju.edu.cn>
2022-05-08 21:05:57 +0800

    Iteration

--- PracticeByCategories/BinaryTree/701_InsertIntoBST/Solution.java
+++ PracticeByCategories/BinaryTree/701_InsertIntoBST/Solution.java
@@ -20,14 +20,24 @@ class Solution {
     }
 
     public TreeNode insertIntoBST(TreeNode root, int val) {
-        if (root == null) {
-            return new TreeNode(val);
+        boolean direction = true; //true代表current节点为pre节点的右子节点，否则为左子节点
+        TreeNode sentinel = new TreeNode(-1, null, root);
+        TreeNode pre = sentinel, current = root;
+        while (current != null) {
+            pre = current;
+            if (current.val > val) {
+                direction = false;
+                current = current.left;
+            } else {
+                direction = true;
+                current = current.right;
+            }
         }
-        if (root.val > val) {
-            root.left = insertIntoBST(root.left, val);
+        if (direction) {
+            pre.right = new TreeNode(val);
         } else {
-            root.right = insertIntoBST(root.right, val);
+            pre.left = new TreeNode(val);
         }
-        return root;
+        return sentinel.right;
     }
 }
