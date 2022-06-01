7bb0c432110d890c0d08793aeedfbc6db75a1f23
LGH <3170105339@zju.edu.cn>
2022-06-02 00:04:40 +0800

    Greedy Algorithm

--- PracticeByCategories/GreedyAlgorithm/122_MaxProfit/Solution.java
+++ PracticeByCategories/GreedyAlgorithm/122_MaxProfit/Solution.java
@@ -1,14 +1,11 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        if (prices.length == 0) {
-            return 0;
+        int result = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] - prices[i - 1] > 0) {
+                result += prices[i] - prices[i - 1];
+            }
         }
-        int withStock = -1 * prices[0], withoutStock = 0;
-        for (int price : prices) {
-            int temp = withStock;
-            withStock = Math.max(withoutStock - price, withStock);
-            withoutStock = Math.max(temp + price, withoutStock);
-        }
-        return withoutStock;
+        return result;
     }
 }
