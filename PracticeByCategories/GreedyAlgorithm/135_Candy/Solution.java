79414326b1a838a1cda77737c14e7250974eb24b
LGH <3170105339@zju.edu.cn>
2022-06-07 00:21:23 +0800

    Single Traverse

--- PracticeByCategories/GreedyAlgorithm/135_Candy/Solution.java
+++ PracticeByCategories/GreedyAlgorithm/135_Candy/Solution.java
@@ -1,28 +1,36 @@
 class Solution {
     public int candy(int[] ratings) {
-        int[] candyNums = new int[ratings.length];
-        for (int i = 0; i < ratings.length; i++) {
-            if (i > 0 && ratings[i] > ratings[i - 1]) {
-                candyNums[i] = candyNums[i - 1] + 1;
+        int pre = 1;
+        int result = 1;
+        int descCount = 0, incCount = 1;
+        for (int i = 1; i < ratings.length; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                if (pre == 1) {
+                    incCount = 1;
+                }
+                descCount = 0;
+                pre += 1;
+                incCount += 1;
+                result += pre;
+            } else if (ratings[i] < ratings[i - 1]) {
+                pre = 1;
+                descCount += 1;
+                if (descCount == incCount) {
+                    descCount += 1;
+                }
+                result += descCount;
             } else {
-                candyNums[i] = 1;
+                pre = 1;
+                descCount = 0;
+                incCount = 1;
+                result += 1;
             }
-        }
-        int result = 0;
-        int rightCount = 1;
-        for (int i = ratings.length - 1; i >= 0; i--) {
-            if (i < ratings.length - 1 && ratings[i] > ratings[i + 1]) {
-                rightCount += 1;
-            } else {
-                rightCount = 1;
-            }
-            result += Math.max(candyNums[i], rightCount);
         }
         return result;
     }
 
     public static void main(String[] args) {
-        int[] ratings = {1, 2, 2};
+        int[] ratings = {1, 2, 87, 87, 87, 2, 1};
         Solution solution = new Solution();
         System.out.println(solution.candy(ratings));
     }
