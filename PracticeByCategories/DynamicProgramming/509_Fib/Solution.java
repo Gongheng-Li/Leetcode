a85f54ee9a085a44b68a79ce3b00d8ade8dd8157
LGH <3170105339@zju.edu.cn>
2022-06-16 22:25:01 +0800

    Quick Power

--- PracticeByCategories/DynamicProgramming/509_Fib/Solution.java
+++ PracticeByCategories/DynamicProgramming/509_Fib/Solution.java
@@ -3,11 +3,33 @@ class Solution {
         if (n == 0 || n == 1) {
             return n;
         }
-        int prev = 0, curr = 1;
-        for (int i = 2; i <= n; i++) {
-            curr = curr + prev;
-            prev = curr - prev;
+        return matrixPow(n - 1)[0][0];
+    }
+
+    private int[][] matrixPow(int pow) {
+        int[][] result = {{1, 0}, {0, 1}};
+        int[][] base = {{1, 1}, {1, 0}};
+        while (pow > 0) {
+            if ((pow & 1) == 1) {
+                result = matrixMul(result, base);
+            }
+            pow >>= 1;
+            base = matrixMul(base, base);
         }
-        return curr;
+        return result;
+    }
+
+    private int[][] matrixMul(int[][] matrix1, int[][] matrix2) {
+        int[][] result = new int[2][2];
+        result[0][0] = matrix1[0][0] * matrix2[0][0] + matrix1[0][1] * matrix2[1][0];
+        result[0][1] = matrix1[0][0] * matrix2[0][1] + matrix1[0][1] * matrix2[1][1];
+        result[1][0] = matrix1[1][0] * matrix2[0][0] + matrix1[1][1] * matrix2[1][0];
+        result[1][1] = matrix1[1][0] * matrix2[0][1] + matrix1[1][1] * matrix2[1][1];
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.fib(4));
     }
 }
