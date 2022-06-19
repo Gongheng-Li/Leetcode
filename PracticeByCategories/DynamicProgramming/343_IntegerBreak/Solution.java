a08b96ee2d62b7e679ab460eba9b0207f11ad7d0
LGH <3170105339@zju.edu.cn>
2022-06-19 17:10:40 +0800

    Improved Dynamic Programming

--- PracticeByCategories/DynamicProgramming/343_IntegerBreak/Solution.java
+++ PracticeByCategories/DynamicProgramming/343_IntegerBreak/Solution.java
@@ -1,19 +1,18 @@
 class Solution {
     public int integerBreak(int n) {
+        if (n < 4) {
+            return n - 1;
+        }
         int[] maxes = new int[n + 1];
-        maxes[1] = 1;
-        for (int i = 2; i <= n; i++) {
-            int max = 0;
-            for (int j = 1; j < i; j++) {
-                max = Math.max(max, j * Math.max(maxes[i - j], i - j));
-            }
-            maxes[i] = max;
+        maxes[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            maxes[i] = Math.max(2 * Math.max(maxes[i - 2], i - 2), 3 * Math.max(maxes[i - 3], i - 3));
         }
         return maxes[n];
     }
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.integerBreak(2));
+        System.out.println(s.integerBreak(10));
     }
 }
