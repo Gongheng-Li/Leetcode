43eaab302f3a920a3dc98b30e38b2c2b7e09fdce
LGH <3170105339@zju.edu.cn>
2022-06-26 20:30:35 +0800

    Dynamic Programming

---
+++ PracticeByCategories/DynamicProgramming/518_Change/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for (int coin : coins) {
+            for (int j = coin; j <= amount; j++) {
+                dp[j] += dp[j - coin];
+            }
+        }
+        return dp[amount];
+    }
+
+    public static void main(String[] args) {
+        int[] coins = {2};
+        Solution solution = new Solution();
+        System.out.println(solution.change(3, coins));
+    }
+}
