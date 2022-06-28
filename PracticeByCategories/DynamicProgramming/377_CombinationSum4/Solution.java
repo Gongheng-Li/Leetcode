7aea94de47c0006ee2773c6a2221d550a6991490
LGH <3170105339@zju.edu.cn>
2022-06-28 21:45:06 +0800

    Dynamic Programming

---
+++ PracticeByCategories/DynamicProgramming/377_CombinationSum4/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int combinationSum4(int[] nums, int target) {
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        for (int i = 0; i <= target; i++) {
+            for (int num : nums) {
+                if (num <= i) {
+                    dp[i] += dp[i - num];
+                }
+            }
+        }
+        return dp[target];
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {1, 2, 3};
+        System.out.println(solution.combinationSum4(nums, 4));
+    }
+}
