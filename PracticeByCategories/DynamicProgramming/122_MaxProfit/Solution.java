38dee3ab986654a8e23547c7e28f445cbd1f202b
LGH <3170105339@zju.edu.cn>
2022-07-03 19:26:00 +0800

    Dynamic Programming

--- PracticeByCategories/DynamicProgramming/122_MaxProfit/Solution.java
+++ PracticeByCategories/DynamicProgramming/122_MaxProfit/Solution.java
@@ -1,11 +1,11 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int maxProfit = 0;
+        int holdStock = -1 * prices[0], holdNothing = 0;
         for (int i = 1; i < prices.length; i++) {
-            if (prices[i] > prices[i - 1]) {
-                maxProfit += prices[i] - prices[i - 1];
-            }
+            int temp = Math.max(holdStock + prices[i], holdNothing);
+            holdStock = Math.max(holdStock, holdNothing - prices[i]);
+            holdNothing = temp;
         }
-        return maxProfit;
+        return holdNothing;
     }
 }
