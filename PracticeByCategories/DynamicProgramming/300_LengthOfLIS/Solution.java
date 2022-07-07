60b89287cef34e85eae1746e465e86e28e016808
LGH <3170105339@zju.edu.cn>
2022-07-07 23:34:30 +0800

    Greedy Algorithm

--- PracticeByCategories/DynamicProgramming/300_LengthOfLIS/Solution.java
+++ PracticeByCategories/DynamicProgramming/300_LengthOfLIS/Solution.java
@@ -2,22 +2,32 @@ import java.util.Arrays;
 
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int[] dp = new int[nums.length];
+        int[] tailOfLength = new int[nums.length];
         int maxLength = 0;
-        Arrays.fill(dp, 1);
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[i] > nums[j]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
+        for (int num : nums) {
+            if (maxLength == 0 || num > tailOfLength[maxLength - 1]) {
+                tailOfLength[maxLength] = num;
+                maxLength += 1;
+            } else {
+                int left = 0, right = maxLength - 1;
+                while (left <= right) {
+                    int middle = (left + right) / 2;
+                    if (tailOfLength[middle] >= num) {
+                        right = middle - 1;
+                    } else {
+                        left = middle + 1;
+                    }
                 }
+                tailOfLength[left] = num;
             }
-            maxLength = Math.max(maxLength, dp[i]);
         }
         return maxLength;
     }
 
     public static void main(String[] args) {
-        int[] nums = {1, 3, 6, 7, 9, 4, 10, 5, 6};
+//        int[] nums = {1, 3, 6, 7, 9, 4, 10, 5, 6};
+//        int[] nums = {0, 1, 0, 3, 2, 3};
+        int[] nums = {4, 10, 4, 3, 8, 9};
         Solution solution = new Solution();
         System.out.println(solution.lengthOfLIS(nums));
     }
