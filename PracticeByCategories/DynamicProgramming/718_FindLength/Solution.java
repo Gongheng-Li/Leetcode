a942a24e6c132422423babe89bb61b148b52a29d
LGH <3170105339@zju.edu.cn>
2022-07-09 00:39:45 +0800

    Sliding Window

--- PracticeByCategories/DynamicProgramming/718_FindLength/Solution.java
+++ PracticeByCategories/DynamicProgramming/718_FindLength/Solution.java
@@ -1,22 +1,29 @@
 class Solution {
     public int findLength(int[] nums1, int[] nums2) {
-        if (nums1.length > nums2.length) {
+        if (nums1.length < nums2.length) {
             return findLength(nums2, nums1);
         }
-        int[] dp = new int[nums1.length];
+        int m = nums1.length, n = nums2.length;
         int maxLength = 0;
-        for (int num : nums2) {
-            for (int j = nums1.length - 1; j >= 0; j--) {
-                if (nums1[j] == num) {
-                    if (j > 0) {
-                        dp[j] = dp[j - 1] + 1;
-                    } else {
-                        dp[j] = 1;
-                    }
-                    maxLength = Math.max(maxLength, dp[j]);
-                } else {
-                    dp[j] = 0;
-                }
+        for (int i = 1; i <= n; i++) {
+            maxLength = Math.max(maxLength, maxInOverlap(nums1, 0, i, nums2, n - i, n));
+        }
+        for (int i = 1; i < m; i++) {
+            int end1 = Math.min(i + n, m);
+            int end2 = Math.min(n, m - i);
+            maxLength = Math.max(maxLength, maxInOverlap(nums1, i, end1, nums2, 0, end2));
+        }
+        return maxLength;
+    }
+
+    private int maxInOverlap(int[] nums1, int start1, int end1, int[] nums2, int start2, int end2) {
+        int count = 0, maxLength = 0;
+        for (int i = start1, j = start2; i < end1; i++, j++) {
+            if (nums1[i] == nums2[j]) {
+                count += 1;
+                maxLength = Math.max(maxLength, count);
+            } else {
+                count = 0;
             }
         }
         return maxLength;
