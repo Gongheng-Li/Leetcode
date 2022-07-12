4964aa7d48a630f14e553d0c13163e813e97599b
LGH <3170105339@zju.edu.cn>
2022-07-12 21:56:52 +0800

    Dynamic Programming

--- PracticeByCategories/DynamicProgramming/392_IsSubsequence/Solution.java
+++ PracticeByCategories/DynamicProgramming/392_IsSubsequence/Solution.java
@@ -6,19 +6,28 @@ class Solution {
         if (s.length() == t.length()) {
             return s.equals(t);
         }
-        int i = 0, j = 0;
-        while (i < s.length() && j < t.length()) {
-            char sChar = s.charAt(i);
-            while (t.charAt(j) != sChar) {
-                j += 1;
-                if (j >= t.length()) {
-                    return false;
+        int sLength = s.length(), tLength = t.length();
+        int[][] dp = new int[tLength + 1][26];
+        for (int i = 0; i < 26; i++) {
+            dp[tLength][i] = -1;
+        }
+        for (int i = tLength - 1; i >= 0; i--) {
+            for (int j = 0; j < 26; j++) {
+                if (t.charAt(i) == j + 'a') {
+                    dp[i][j] = i;
+                } else {
+                    dp[i][j] = dp[i + 1][j];
                 }
             }
-            i += 1;
-            j += 1;
         }
-        return i == s.length();
+        int index = 0;
+        for (int i = 0; i < sLength; i++) {
+            if (dp[index][s.charAt(i) - 'a'] == -1) {
+                return false;
+            }
+            index = dp[index][s.charAt(i) - 'a'] + 1;
+        }
+        return true;
     }
 
     public static void main(String[] args) {
